507fe28d7e5885f5d2ae1e589504b4e06d119bab
Anton <azezin@inbox.ru>
2022-09-29 13:50:58 +0300

    Better code style and fixed some small bugs

--- inc/error_handling/error_handling.h
+++ inc/error_handling/error_handling.h
@@ -1,37 +1,46 @@
 
 /// @file error_handling.h
-/// @brief Macros for convinient error monitoring in debug
+/// @brief Macros for convinient error monitoring
 
 #include <stdio.h>
 #include <stdint.h>
 
 #include "special_macros.h"
 
-/// Types of err_code variable
+#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
+
+/// @brief Struct for pointer variable data
+typedef struct
+{
+    void*   stk_ptr   = NULL; ///< pointer value
+    char*   var_name  = NULL; ///< name of variable
+    char*   func_name = NULL; ///< name of func where variable defined
+    char*   file      = NULL; ///< name of file where function defined
+    size_t  line      = 0;    ///< number of line where this data loads
+} VAR_DATA;
+
+#endif /* ERROR_HANDLING_H */
+
+/// @brief Fills VAR_DATA
+#define VAR_INFO(var) {var, #var + (#var[0] == '&'), __PRETTY_FUNCTION__, __FILE__, __LINE__}
 
 /// @brief Prints string error message in stderr
 
-#define ERR_REPORT_MSSG(msg)  \
-fprintf(stderr,              \
-        "Error: %s. "        \
-        "In line %d "        \
-        "in func %s\n",      \
-        msg,                 \
-        __LINE__,            \
-        __PRETTY_FUNCTION__)
-
-/// @brief Like ERR_REPORT_MSSG but prints multi-char
-/// constant err_code instead of string message
-
-#define ERR_REPORT_CODE(code)        \
-fprintf(stderr,                      \
-        "Error: %.*s. "              \
-        "In line %d "                \
-        "in func %s\n",              \
-        (int) sizeof(code),          \
-        (const char*) &code,         \
-        __LINE__,                    \
-        __PRETTY_FUNCTION__)
+#ifdef NDEBUG
+    #define ERR_REPORT_MSSG(stream, msg) 1
+#else
+    #define ERR_REPORT_MSSG(stream, msg) \
+        fprintf(stream,                  \
+                "Error: %s. "            \
+                "in file: %s "           \
+                "in line: %d "           \
+                "in func %s\n",          \
+                msg,                     \
+                __FILE__,                \
+                __LINE__,                \
+                __PRETTY_FUNCTION__)
+#endif
 
 /// @brief Provides error support mode for functions
 ///
@@ -41,67 +50,26 @@ fprintf(stderr,                      \
 #define ERR_SUPPORT_DEFN , ERR_TYPE *err
 
 /// @brief Sets err to given value
-#define ERR_SET(new_err) (err) ? *err = new_err : 1
+#define ERR_SET(new_err) (err) ? *err |= new_err : 1
 
 /// @brief Checks condition, updates err value and 
 /// (if NDEBUG is not defined) prints error message
-
-#ifdef NDEBUG
-    #define ERR_HANDLE_MSSG(cond, new_err, ERR_MSSG) \
-    (cond) &&                                      \
-    (ERR_SET(new_err))
-#else
-    #define ERR_HANDLE_MSSG(cond, new_err, ERR_MSSG)     \
-    (cond) &&                                          \
-    EVAL1(DEFER1(ERR_REPORT_MSSG)(ERR_MSSG(new_err))) && \
-    (ERR_SET(new_err))
-#endif
-
-/// @brief Like ERR_HANDLE_MSSG but prints multi-char
-/// constant err_code instead of string message
-
-#ifdef NDEBUG
-    #define ERR_HANDLE_CODE(cond, new_err, ERR_CODE) \
-    (cond) &&                                        \
-    (ERR_SET(new_err))
-#else
-    #define ERR_HANDLE_CODE(cond, new_err, ERR_CODE)     \
-    (cond) &&                                            \
-    EVAL1(DEFER1(ERR_REPORT_CODE)(ERR_CODE(new_err))) && \
-    (ERR_SET(new_err))
-#endif
+#define ERR_HANDLE_MSSG(stream, cond, new_err)                       \
+    if (cond)                                                        \
+    {                                                                \
+        EVAL1(DEFER1(ERR_REPORT_MSSG)(stream, CAT(new_err, _MSSG))); \
+        ERR_SET(new_err);                                            \
+    }
 
 /// @brief Like ERR_HANDLE_MSSG but suffix version
-
-#ifdef NDEBUG
-    #define ERR_HANDLED_MSSG(new_err, ERR_MSSG) \
-    && (ERR_SET(new_err))
-#else
-    #define ERR_HANDLED_MSSG(new_err, ERR_MSSG)      \
-    && EVAL1(DEFER1(ERR_REPORT_MSSG)(ERR_MSSG(msg))) \
+#define ERR_HANDLED_MSSG(stream, new_err)                          \
+    && EVAL1(DEFER1(ERR_REPORT_MSSG)(stream, CAT(new_err, _MSSG))) \
     && (ERR_SET(new_err))   
-#endif
-
-/// @brief Like ERR_HANDLE_CODE but suffix version
-
-#ifdef NDEBUG
-    #define ERR_HANDLED_CODE(new_err, ERR_CODE) \
-    ERR_HANDLED_MSSG(new_err, "")
-#else
-    #define ERR_HANDLED_CODE(new_err, ERR_CODE)          \
-    && EVAL1(DEFER1(ERR_REPORT_CODE)(ERR_CODE(new_err))) \
-    && (ERR_SET(new_err)) 
-#endif
-
-/// @brief Checks if err is not 0 and reports its code
-#define ERR_CHECK_CODE(err, ERR_CODE)         \
-(err) &&                                      \
-EVAL1(DEFER1(ERR_REPORT_CODE)(ERR_CODE(err)))
 
 /// @brief Checks if err is not 0 and reports its message
-#define ERR_CHECK_MSSG(err, ERR_MSSG)           \
-(err) &&                                      \
-EVAL1(DEFER1(ERR_REPORT_MSSG)(ERR_MSSG(err)))
+#define ERR_CHECK_MSSG(stream, check_err, compare_err)              \
+    (check_err & compare_err) &&                                    \
+    EVAL1(DEFER1(ERR_REPORT_MSSG)(stream, CAT(compare_err, _MSSG)))
 
 /// @brief Checks condition and, if false, prints error message
 ///
@@ -110,16 +78,15 @@ EVAL1(DEFER1(ERR_REPORT_MSSG)(ERR_MSSG(err)))
 #ifdef NDEBUG
     #define ASSERT_MSSG(cond, msg) ((void)0)
 #else
-    #define ASSERT_MSSG(cond, msg) \
-    if (!(cond))                  \
-        ERR_REPORT_MSSG(msg);
+    #define ASSERT_MSSG(cond, msg)                 \
+        if (!(cond)) ERR_REPORT_MSSG(stderr, msg);
 #endif
 
 /// @brief Like ASSERT_MSSG but arg msg is optional (#cond by default)
-#define ASSERT(cond, ...)                    \
-GET_ARG_3( ,   ##__VA_ARGS__,                \
-ASSERT_MSSG(cond, __VA_ARGS__),               \
-ASSERT_MSSG(cond, "failed condition: "#cond))
+#define ASSERT(cond, ...)                         \
+    GET_ARG_3( ,   ## __VA_ARGS__,                \
+    ASSERT_MSSG(cond, __VA_ARGS__),               \
+    ASSERT_MSSG(cond, "failed condition: "#cond))
 
 /// @brief Redefinition std assert for compatibility
 
@@ -129,9 +96,4 @@ ASSERT_MSSG(cond, "failed condition: "#cond))
 #define assert(cond) ASSERT(cond)
 
 /// @brief Suffix version of ASSERT
-
-#ifdef NDEBUG
-    #define ASSERTED(msg) || 0
-#else
-    #define ASSERTED(msg) || ERR_REPORT_MSSG(msg)
-#endif
+#define ASSERTED(stream, msg) || ERR_REPORT_MSSG(stream, msg)
--- main.cpp
+++ main.cpp
@@ -8,23 +8,34 @@
 
 #include "inc/error_handling/error_handling.h"
 
-#include <string.h>
+int compare_lines_pointers(const void* a, const void* b);
 
-int compare_lines_pointers(const void *a, const void *b);
+int main(int argc, const char **argv)
+{
+    const char *i_file_name = NULL;
+    const char *o_file_name = NULL;
 
-int main(int argc, char **argv) {
-    const char *i_file_name = argv[argc - 2];
-    const char *o_file_name = argv[argc - 1];
-
-    setlocale(LC_ALL, "ru_RU.UTF-8");
-    Text *text = construct_text_file(i_file_name, &part_line);
-    if (text == NULL) {
-        ERR_REPORT_MSSG("cannot construct text");
+    if (argc == 3) 
+    {
+        i_file_name = argv[1];
+        o_file_name = argv[2];
+    }
+    else
+    {
+        fprintf(stderr, "Wrong number of arguments given");
+        return 1;
     }
 
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    
+    Text *text = construct_text_file(i_file_name, &part_line);
+    if (text == NULL) 
+        fprintf(stderr, "cannot construct text");
+
     FILE *file_out = fopen(o_file_name, "w");
-    if (file_out == NULL) {
-        ERR_REPORT_MSSG("cannot open output file");
+    if (file_out == NULL)
+    {
+        fprintf(stderr, "cannot open output file");
         destruct_text(text);
         return 1;
     }
@@ -33,6 +44,7 @@ int main(int argc, char **argv) {
                text->n_parts,
                text->part->size,
                compare_lines_lex_utf8);
+    
     print_all_parts_stream(file_out, text);
     fprintf(file_out, "\n*****************************************\n");
 
@@ -40,6 +52,7 @@ int main(int argc, char **argv) {
                text->n_parts,
                text->part->size,
                compare_lines_reverse_lex_utf8);
+    
     print_all_parts_stream(file_out, text);
     fprintf(file_out, "\n*****************************************\n");
 
@@ -47,13 +60,16 @@ int main(int argc, char **argv) {
                text->n_parts,
                text->part->size,
                compare_lines_pointers);
+    
     print_all_parts_stream(file_out, text);
 
     destruct_text(text);
     fclose(file_out);
+    
     return 0;
 }
 
-int compare_lines_pointers(const void *a, const void *b) {
-    return (int) (((const Line*) a)->start - ((const Line*) b)->start);
+int compare_lines_pointers(const void* a, const void* b) {
+    return (int) (((const Line*) a)->start -
+                  ((const Line*) b)->start);
 }
